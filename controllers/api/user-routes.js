85a8395b5ff671e9392dae5cc21375b66bea1545
Jack Doran <jcdoran33@gmail.com>
2022-06-29 16:29:42 -0400

    set up framework with imports and comments in user-routes.js

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,17 @@
+//imports - router, and any model necessary
+const router = require("express").Router();
+const { User } = require("../../models");
+
+//Endpoint: "/api/users"
+
+//get / display all users
+
+
+
+//get / display a certain user
+
+
+//update a certain user
+
+
+//delete a certain user
