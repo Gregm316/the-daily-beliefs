3cf42d6d9fdbbfc7eb5670cffcdab8b5d8f8b5eb
Jack Doran <jcdoran33@gmail.com>
2022-06-29 16:36:13 -0400

    added / and /:id routes to user-routes.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -4,12 +4,31 @@ const { User } = require("../../models");
 
 //Endpoint: "/api/users"
 
-//get / display all users
-
+//get / display all users - endpoint: "/api/users/"
+router.get("/", async (req, res) => {
+    // try catch, use findAll for User
+    try{ 
+        const userData = await User.findAll ({
+            attributes: { exclude: ["password"]},
+            order: [['name', 'ASC']],
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 //get / display a certain user
-
+router.get("/:id", async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.params.id, {
+            attributes: { exclude: ["password"]},
+            order: [['name', 'ASC']],
+        })
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 //update a certain user
 
