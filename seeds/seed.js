21ba790ef8c1d28e722bbe9ced7f6e44ee9ec96e
Jack Doran <jcdoran33@gmail.com>
2022-06-29 13:32:35 -0400

    created and completed the seed.js file - sample data still currently empty

---
+++ seeds/seed.js
@@ -0,0 +1,32 @@
+//import Sequelize, and Models
+const sequelize = require("../config/connection");
+const { User, Character , Post} = require("../models");
+
+const userData = require("./userData.json");
+const characterData = require("./characterData.json");
+const postData = require("./postData.json");
+
+//define function to seed database
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    await Post.bulkCreate(postData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    await Character.bulkCreate(characterData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    process.exit(0);
+};
+
+//call seedDatabase function
+seedDatabase();
