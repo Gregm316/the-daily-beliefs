e1a3decdba77e15fc68472b8f7abb4f4695ac4cc
Jack Doran <jcdoran33@gmail.com>
2022-06-29 13:21:17 -0400

    updated connection.js with if/else and JAWS DB statement

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,21 @@
-//import Sequelize, and the dotenv package
-const Sequelize = require("sequelize");
-require("dotenv").config();
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
-//define sequelize
-const sequelize = new Sequelize(
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host: "127.0.0.1",
-        dialect: "mysql",
-        port: 3306
+      host: '127.0.0.1',
+      dialect: 'mysql',
+      port: 3306
     }
-);
+  );
+}
 
-//export "sequelize"
 module.exports = sequelize;
