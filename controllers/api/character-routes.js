903af75c95e2706f1c66d238cde8420904e390dd
Jack Doran <jcdoran33@gmail.com>
2022-06-29 18:41:56 -0400

    added package: connect-session-sequelize

--- controllers/api/character-routes.js
+++ controllers/api/character-routes.js
@@ -7,7 +7,7 @@ const { Character, Post } = require("../../models");
 //get / display ALL character (use findAll)
 
 
-//get route - display a certain character by id# (use findbyPk)
+//get route - display a certain character by id# (use findbyPk) (we would essentially use Post.findAll where character_id=character.id)
 
 //post route - only be used if we allow the end user to creat a new character page for a marvel character that we did not hard code
 
