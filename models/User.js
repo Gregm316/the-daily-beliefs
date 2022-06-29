ba70e2d03d75268eb364fc7cb89347b0068cbe4b
Jack Doran <jcdoran33@gmail.com>
2022-06-29 11:14:43 -0400

    filled out User model, completed column names and attributes

--- models/User.js
+++ models/User.js
@@ -1,3 +1,49 @@
 //import model, satatypes
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
+
+class User extends Model {}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8,20]
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+)
+
+
+//export the model User
+module.exports = User;
