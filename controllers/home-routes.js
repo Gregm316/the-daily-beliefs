e9896f09d0199bd7e450b95adeaf5d7feb2a07c2
Jack Doran <jcdoran33@gmail.com>
2022-06-29 16:03:26 -0400

    added login route and / route into home-routes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,7 +6,31 @@ const router = require("express").Router();
 const withAuth = require("../utils/auth");
 
 //lay out the routes here - should have "/" and "/login"
-router.get("/", withAuth, )
+
+//main "/" route
+router.get("/", withAuth, async (req, res) => {
+    //do we need to display any info fromt he tables (for logged in user)? I think we just want blank homepage
+    //if we wanted to display, this is where we would do something like a Post.findAll(...)
+    try {
+        //render the homepage hamdlebars
+        render("homepage", {
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//login route
+router.get("/login", (req, res) => {
+    //if the user is logged in, redirect to the homepage "/"...
+    if (req.session.logged_in) {
+        res.redirect("/");
+        return;
+    }
+    //...if they are not logged in, then render the login handlebar
+    res.render("login");
+})
 
 //export router
 module.exports = router;
