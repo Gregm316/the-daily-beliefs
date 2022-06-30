c1a10828e7bfaacc2e70b0d33e1eb1c4ba5a5593
gregm316 <gregm316@gmail.com>
2022-06-30 12:22:09 -0400

    Updated user routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,48 @@
+//imports - router, and any model necessary
+const router = require("express").Router();
+const { User } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+//Endpoint: "/api/users"
+
+//get / display all users - endpoint: "/api/users/"
+router.get("/", withAuth, async (req, res) => {
+    // try catch, use findAll for User
+    try{ 
+        const userData = await User.findAll ({
+            attributes: { exclude: ["password"]},
+            order: [['last_name', 'ASC']],
+        });
+        res.status(200).json(userData); //do we need to switch this to a res.render instead??
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+//get / display a certain user
+router.get("/:id", withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.params.id, {
+            attributes: { exclude: ["password"]},
+            order: [['last_name', 'ASC']],
+        })
+        //if no user with that ID, send error message
+        if (!userData) {
+            res.status(404).json({ message: "No user with that ID"});
+            return;
+        }
+        res.status(200).json(userData); //do we need to switch this to a res.render instead??
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+//update a certain user
+
+
+//delete a certain user
+
+//export router
+module.exports = router;
