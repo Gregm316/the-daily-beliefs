ee65a009e3cf065f03bfe8e36a9de1bdc9aa4a97
Jack Doran <jcdoran33@gmail.com>
2022-06-30 14:38:02 -0400

    minor updates to character-routes. Added display commands (innerHTML) to render fetched data to the page

--- controllers/api/character-routes.js
+++ controllers/api/character-routes.js
@@ -2,11 +2,11 @@
 const router = require("express").Router();
 const { Character, Post } = require("../../models");
 const withAuth = require("../../utils/auth");
-require('dotenv').config();
+require('dotenv').config(); //import dotenv so we can hide API key
 
 const imgElement = document.querySelector("#img-placeholder"); //added this if we want to update the character's thumbnail image into the character page being displayed
 const bioElement = document.querySelector("#bio-placeholder"); //added this for same reason, update bio via API
-const apiUrl = "https://gateway.marvel.com/v1/public/characters?name=hulk&ts=test&apikey="+ process.env.API_KEY+"&hash=c3ab47d4e62a9d8621a7777293aa85ee"
+const apiUrl = "https://gateway.marvel.com/v1/public/characters?name=hulk&ts=test&apikey="+ process.env.API_KEY +"&hash=c3ab47d4e62a9d8621a7777293aa85ee"
 
 //Endpoint = "/api/characters"
 
@@ -49,13 +49,15 @@ router.get("/:id", withAuth, async (req, res) => {
             return res.json();
           })
           .then(function(data) {
+            //then save the Bio and Thumbnail as variables...
             let thumbnailImg = data.data.results.thumbnail + ".jpg";
             let bio = data.data.results.description ;
+            //Then use .innerhtml to render that content correct place in our webpage...
+            imgElement.innerHTML(`<img src=${thumbnailImg}/>`)
+            bioElement.innerHTML(bio);
           })
-        //then save the Bio and Thumbnail as variables...
-        //Then use .innerhtml to render that content correct place in our webpage...
 
-        //then continue to grab the rest of the process of querying data from our DB
+        //then continue to grab the rest of the normal process of querying data from our DB
         const characterData = await Character.findByPk(req.params.id, { // find the single character by the given id...
             include: [{ model: Post}] //join the Post table
         });
