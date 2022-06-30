d8bc1cf2475f97cbd405ebcb508c08bd13efa865
Jack Doran <jcdoran33@gmail.com>
2022-06-30 11:32:37 -0400

    added POST route for user-routes.js, for when a new user account is created

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -39,6 +39,21 @@ router.get("/:id", withAuth, async (req, res) => {
     }
 })
 
+//create (POST) a new user
+router.post("/", async (req, res) => {
+    try {
+        const userData = await User.create({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email,
+            password: req.body.password
+        });
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 //update a certain user
 
 
