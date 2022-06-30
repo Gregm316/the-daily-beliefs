3dc1210093c7b83a93b6fbc6a9a371c05dc727f6
Jack Doran <jcdoran33@gmail.com>
2022-06-30 11:00:09 -0400

    added res.json output in routes (until handlebars are finished)

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -12,10 +12,11 @@ router.get("/", async (req, res) => {
     //do we need to display any info fromt he tables (for logged in user)? I think we just want blank homepage
     //if we wanted to display, this is where we would do something like a Post.findAll(...)
     try {
+        res.status(200).json({message: "Testing the home route!"});
         //render the homepage hamdlebars
-        render("homepage", {
-            logged_in: req.session.logged_in
-        });
+        // render("homepage", { // uncomment when handlebars is implemented
+        //     logged_in: req.session.logged_in
+        // });
     } catch (err) {
         res.status(500).json(err);
     }
@@ -28,8 +29,9 @@ router.get("/login", (req, res) => {
         res.redirect("/");
         return;
     }
+    res.status(200).json({message: `testing the /login route with a ${req.method} request!` })
     //...if they are not logged in, then render the login handlebar
-    res.render("login");
+    // res.render("login"); // uncomment when handlebars is implemented
 })
 
 //export router
