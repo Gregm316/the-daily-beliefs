fcf4fda76d6afd954ef9c72d85a82507a5f6acc8
gregm316 <gregm316@gmail.com>
2022-07-01 12:36:44 -0400

    Updated character-routes

--- controllers/api/character-routes.js
+++ controllers/api/character-routes.js
@@ -1,14 +1,11 @@
-
 //imports - router, and any model necessary
 const router = require("express").Router();
 const { Character, Post } = require("../../models");
+const marvelFetch = require("../../public/js/marvelFetch");
 const withAuth = require("../../utils/auth");
 require('dotenv').config(); //import dotenv so we can hide API key
-//require the marvelFetch() function if we go that route
-
-const imgElement = document.querySelector("#img-placeholder"); //added this if we want to update the character's thumbnail image into the character page being displayed
-const bioElement = document.querySelector("#bio-placeholder"); //added this for same reason, update bio via API
-const apiUrl = "https://gateway.marvel.com/v1/public/characters?name=hulk&ts=test&apikey="+ process.env.API_KEY +"&hash=c3ab47d4e62a9d8621a7777293aa85ee"
+//require the marvelFetch() function, if we go that route
+const fetch = require("../../public/js/marvelFetch");
 
 //Endpoint = "/api/characters"
 
@@ -26,41 +23,13 @@ router.get("/", withAuth, async (req, res) => {
     }
 });
 
-//get route - display a certain character by id# (use findbyPk) (we would essentially use Post.findAll where character_id=character.id)
-// router.get("/:id", withAuth, async (req, res) => {
-//     try {
-//         const characterData = await Post.findAll({ // find all items in the Post table but...
-//             where: {
-//                 character_id: req.params.id // ...only return Posts where the id in the url matches the character_id column in the Post table
-//             }
-//         });
-//         if (!characterData) {
-//             res.status(404).json({ message: "No character by that ID number"});
-//         }
-//         res.status(200).json(characterData); //do we need to switch this to a res.render instead??
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
-
-
 //duplicate the above get route - use Character.findByPk, and includ: [{ model: Post}] where: character_id = character.id
 router.get("/:id", withAuth, async (req, res) => {
     console.log("GET request on /api/characters/:id");
     try {
+        
         //try to fetch the given character's URL (maybe as an external function)....
-        fetch(apiUrl)
-        .then(function(res){
-            return res.json();
-          })
-          .then(function(data) {
-            //then save the Bio and Thumbnail as variables...
-            let thumbnailImg = data.data.results.thumbnail + ".jpg";
-            let bio = data.data.results.description ;
-            //Then use .innerhtml to render that content correct place in our webpage...
-            imgElement.innerHTML(`<img src=${thumbnailImg}/>`)
-            bioElement.innerHTML(bio);
-          })
+        const fetchChar = await marvelFetch();
 
         //then continue to grab the rest of the normal process of querying data from our DB
         const characterData = await Character.findByPk(req.params.id, { // find the single character by the given id...
