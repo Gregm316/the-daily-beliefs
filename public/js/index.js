f0cfc37d58dc60d4d066872718fd3b737ee02047
Jack Doran <jcdoran33@gmail.com>
2022-07-04 19:09:05 -0400

    minor edits to index.js in pulic folder

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const path = require("path");
 const { Character, Post } = require("../../models");
 
-//purpose of this tile is to try to redirect the user to the appropriate character page, based on their selection from the drop down...
+//purpose of this file is to try to redirect the user to the appropriate character page, based on their selection from the drop down...
 
 let dropdown = document.querySelector("#character-selection");
 
@@ -22,10 +22,10 @@ function routeCreator(selectedChar) {
             charId = await Character.findOne({
                 attributes: {id},
                 where: {
-                    character_name: selectedChar //where the character name matches character anme from table
+                    character_name: selectedChar //where the character name matches character name from table
                 }
             });
-            //then change the location to new URL
+            //then change the location to new URL...maybe use res.redirect instead
             location.href = `http://localhost:3001/characters/${chardId}` //not sure if this is correct
         } catch (err) {
             console.log(err);
