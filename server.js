ff1aad0bf3504617012877601d06cb4dafd5b012
Jack Doran <jcdoran33@gmail.com>
2022-07-05 10:21:28 -0400

    updated from greg's code

--- server.js
+++ server.js
@@ -36,6 +36,72 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
+// -------------------Beginning of chat code-----------------
+
+const formatMessage = require('./helpers/formatDate')
+const {
+  getActiveUser,
+  exitRoom,
+  newUser,
+  getIndividualRoomUsers
+} = require('./helpers/userHelper.js');
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+// this block will run when the client connects
+io.on('connection', socket => {
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = newUser(socket.id, username, room);
+
+    socket.join(user.room);
+
+    // General welcome
+    // socket.emit('message', formatMessage('Messages are limited to this room!'));
+
+    // Broadcast everytime users connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(`${user.username} has joined the room`)
+      );
+
+    // Current active users and room name
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getIndividualRoomUsers(user.room)
+    });
+  });
+
+  // Listen for client message
+  socket.on('chatMessage', msg => {
+    const user = getActiveUser(socket.id);
+
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    const user = exitRoom(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(`${user.username} has left the room`)
+      );
+
+      // Current active users and room name
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getIndividualRoomUsers(user.room)
+      });
+    }
+  });
+});
+
+// ----------------End of chat code------------------------
+
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
