309be1a81ea104512eefae341e38de0f9a4ecc2f
gregm316 <gregm316@gmail.com>
2022-07-05 21:30:04 -0400

    Updated user-routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -10,9 +10,9 @@ const withAuth = require("../../utils/auth");
 router.get("/", withAuth, async (req, res) => {
     console.log("GET request on /api/users");
     // try catch, use findAll for User
-    try{ 
-        const userData = await User.findAll ({
-            attributes: { exclude: ["password"]},
+    try {
+        const userData = await User.findAll({
+            attributes: { exclude: ["password"] },
             order: [['last_name', 'ASC']],
         });
         res.status(200).json(userData); //do we need to switch this to a res.render instead??
@@ -28,12 +28,12 @@ router.get("/:id", withAuth, async (req, res) => {
     console.log("GET request on /api/users/:id");
     try {
         const userData = await User.findByPk(req.params.id, {
-            attributes: { exclude: ["password"]},
+            attributes: { exclude: ["password"] },
             order: [['last_name', 'ASC']],
         })
         //if no user with that ID, send error message
         if (!userData) {
-            res.status(404).json({ message: "No user with that ID"});
+            res.status(404).json({ message: "No user with that ID" });
             return;
         }
         res.status(200).json(userData); //do we need to switch this to a res.render instead??
@@ -45,7 +45,7 @@ router.get("/:id", withAuth, async (req, res) => {
 })
 
 //create (POST) a new user
-router.post("/", async (req, res) => {
+router.post("/signup", async (req, res) => {
     console.log("POST request on /api/users");
     try {
         const userData = await User.create({
@@ -55,7 +55,57 @@ router.post("/", async (req, res) => {
             username: req.body.username,
             password: req.body.password
         });
-        res.status(200).json(userData);
+
+        req.session.save(() => {
+            req.session.logged_in = true;
+
+            res.status(200).json(userData);
+        });
+
+        // res.status(200).json(userData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+//sign in
+router.post('/login', async (req, res) => {
+    console.log("POST /api/users/login");
+    try {
+        const dbUserData = await User.findOne({
+            where: {
+                email: req.body.email,
+            },
+        });
+
+        if (!dbUserData) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect email or password. Please try again!' });
+            return;
+        }
+
+        const validPassword = await dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect email or password. Please try again!' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.logged_in = true;
+            console.log(
+                '🚀 ~ file: user-routes.js ~ line 57 ~ req.session.save ~ req.session.cookie',
+                req.session.cookie
+            );
+
+            res
+                .status(200)
+                .json({ user: dbUserData, message: 'You are now logged in!' });
+        });
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
