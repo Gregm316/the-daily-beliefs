609e86d2a8d82d65b7cd532be37c02698f995c64
gregm316 <gregm316@gmail.com>
2022-07-06 09:54:25 -0400

    Added code to public character.js

--- public/js/character.js
+++ public/js/character.js
@@ -8,7 +8,31 @@ fetch("/api/characters/")
 })
 .then((data) => {
     console.log("Data: ", data);
-    //do we need command here to redirect to the appropriate caracter page, based on the value of req.session.characterId
-    // location.replace(`/api/characters/${req.session.characterId}`);
-    //
-})
+});
+
+const oldComments = document.getElementById('old-comments');
+const commentText = document.querySelector('.textarea');
+const submitBtn = document.getElementById('submit');
+
+submitBtn.addEventListener("click", newComment);
+
+function newComment() {
+
+    const article = document.createElement("article");
+    article.classList.add('media');
+    oldComments.appendChild(article);
+
+    const div1 = document.createElement("div");
+    div1.classList.add('media-content');
+    article.appendChild(div1);
+
+    const div2 = document.createElement("div");
+    div1.classList.add('content');
+    div1.appendChild(div2);
+    
+    
+    const p = document.createElement('p');
+    p.innerHTML = '<strong>' + 'USERNAME' + '</strong>' + 
+    '<br>' + commentText.value;
+    div2.appendChild(p);
+}
