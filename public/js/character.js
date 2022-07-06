ea80626f2ac34a2d0293562f8310d0cffbcb01b1
Jack Doran <jcdoran33@gmail.com>
2022-07-06 09:54:37 -0400

    added character.js text

--- public/js/character.js
+++ public/js/character.js
@@ -12,3 +12,31 @@ fetch("/api/characters/")
     // location.replace(`/api/characters/${req.session.characterId}`);
     //
 })
+
+
+const oldComments = document.getElementById('old-comments');
+const commentText = document.querySelector('.textarea');
+const submitBtn = document.getElementById('submit');
+
+submitBtn.addEventListener("click", newComment);
+
+function newComment() {
+
+    const article = document.createElement("article");
+    article.classList.add('media');
+    oldComments.appendChild(article);
+
+    const div1 = document.createElement("div");
+    div1.classList.add('media-content');
+    article.appendChild(div1);
+
+    const div2 = document.createElement("div");
+    div1.classList.add('content');
+    div1.appendChild(div2);
+    
+    
+    const p = document.createElement('p');
+    p.innerHTML = '<strong>' + 'Jack' + '</strong>' + 
+    '<br>' + commentText.value;
+    div2.appendChild(p);
+}
