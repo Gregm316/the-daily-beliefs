76cf037fc1daf29889024a80485ce4f4e710fed2
AlyLopez02 <alylopez0217@gmail.com>
2022-07-06 04:05:58 -0400

    got api working if character name is hard coded in

--- public/js/character.js
+++ public/js/character.js
@@ -12,12 +12,37 @@ fetch("/api/characters/")
 })
 
 // My attempt to make marvel API work here
-const characterName = 'hulk'; // hardcoding for now to ensure api works
+const nameSpot = document.getElementById('character-name');
+const descriptionSpot = document.getElementById('character-description');
+const image = document.getElementById('character-image');
 
-const apiUrl = "https://gateway.marvel.com/v1/public/characters?name="+ characterName +"&ts=test&apikey="+ process.env.API_KEY +"&hash=bf87806abd3d8c926aa52d5042ac4a42";
+// NEED TO SOMEHOW GET SELECTED CHARACTER
+const characterName = 'iron%20man'; // hardcoding for now to ensure api works
 
+const apiUrl = "https://gateway.marvel.com/v1/public/characters?name="+ characterName +"&ts=test&apikey="+ "ba472da2d16415fde113b35fbddfc3a3&hash=bf87806abd3d8c926aa52d5042ac4a42";
 
+getCharacterInfo();
 
 function getCharacterInfo() {
     fetch(apiUrl)
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function(data){
+        var name = data.data.results[0].name;
+        var description = data.data.results[0].description;
+        var thumbnailPath = data.data.results[0].thumbnail.path;
+
+        showInfo(name, description, thumbnailPath);
+    })
+}
+
+function showInfo(name, description, thumbnailPath) {
+    nameSpot.innerHTML = name;
+    descriptionSpot.innerHTML = description;
+
+    console.log(thumbnailPath);
+
+    const source = thumbnailPath + '/' + 'landscape_incredible.jpg';
+    image.src = source;
 }
