d388ed0fa6d3cc580351d2a5a4b725b33f474452
gregm316 <gregm316@gmail.com>
2022-07-05 21:28:48 -0400

    Added post-routes and updated character-routes

--- controllers/api/character-routes.js
+++ controllers/api/character-routes.js
@@ -11,35 +11,32 @@ const fetch = require("../../public/js/marvelFetch");
 //Endpoint = "/api/characters"
 
 //get / display ALL character (use findAll)
-router.get("/", withAuth, async (req, res) => {
-    console.log("GET request on /api/characters/");
-    try {
-        const characterData = await Character.findAll({
-            attributes: ["character_name"]
-        });
-        res.status(200).json(characterData); //displays all characterData - do we need to switch this to a res.render instead??
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
-});
+// router.get("/", withAuth, async (req, res) => {
+//     console.log("GET request on /api/characters/");
+//     try {
+//         const characterData = await Character.findAll({
+//             attributes: ["character_name"]
+//         });
+//         res.status(200).json(characterData); //displays all characterData - do we need to switch this to a res.render instead??
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).json(err);
+//     }
+// });
 
 //duplicate the above get route - use Character.findByPk, and includ: [{ model: Post}] where: character_id = character.id
-router.get("/:id", withAuth, async (req, res) => {
-    console.log("GET request on /api/characters/:id");
+router.get("/", withAuth, async (req, res) => {
+    console.log("GET /api/characters/");
+    req.session.characterId = req.body.characterId; //used to be req.params.id, think it should be req.body.characterId. Not 100%
     try {
-        
-        //try to fetch the given character's URL (maybe as an external function)....
-        // const fetchChar = await marvelFetch();
-
-        //then continue to grab the rest of the normal process of querying data from our DB
-        const characterData = await Character.findByPk(req.params.id, { // find the single character by the given id...
-            include: [{ model: Post}] //join the Post table
+        console.log("Character ID: ", req.session.characterId);
+        const dbCharacter = await Character.findOne({
+            include: [{ model: Post }],
+            where: {
+                id: req.session.characterId //where the character id matches character id from table
+            }
         });
-        if (!characterData) {
-            res.status(404).json({ message: "No character by that ID number"});
-        }
-        res.status(200).json(characterData); //do we need to switch this to a res.render instead??
+        res.status(200).json(dbCharacter); //do we want to use res.sendFile instead here? to send the HTML file that matches the characterId value??
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
@@ -47,26 +44,33 @@ router.get("/:id", withAuth, async (req, res) => {
 });
 
 //include a post route the does creates a new post? instead of in post-routes?
-router.post("/:id", withAuth, async (req, res) => {
-    console.log("POST request on route /api/characters/:id");
+router.post("/", withAuth, async (req, res) => {
+    console.log("POST /api/characters");
     try {
         //POST route - create a new post with the character_id of whatever re.params.id is
-        const newPost = await Post.create({
-            message: req.body.message,
-            character_id: req.params.id,
-            user_id: req.body.user_id
-        });
+        // const newPost = await Post.create({
+        //     message: req.body.message,
+        //     character_id: req.params.id,
+        //     user_id: req.body.user_id
+        // });
         //re-run query that finds all posts with the certain ID
-        const characterData = await Character.findByPk(req.params.id, { // find the single character by the given id...
-            include: [{ model: Post }] //join the Post table
-        });
-        if (!characterData) {
-            res.status(404).json({ message: "No character by that ID number"});
-        }
+        // const characterData = await Character.findByPk(req.params.id, { // find the single character by the given id...
+        //     include: [{ model: Post }] //join the Post table
+        // });
+        // if (!characterData) {
+        //     res.status(404).json({ message: "No character by that ID number"});
+        // }
         //create array of all current posts (for that ID), then push the newPost to it
         // const posts = characterData.map((post) => post.get({ plain: true })); //recently commented this out since we change the function
         // characterData.push(newPost); // think this line is uncessary because it is async - when characterData is created here, it shoudl include the newPost just added to the table above
-        res.status(200).json(characterData); //do we need to switch this to a res.render instead??
+        req.session.save(() => {
+            req.session.characterId = req.body.characterId;
+
+            res
+                .status(200)
+                .json({ message: 'Character ID saved.' });
+        });
+
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
